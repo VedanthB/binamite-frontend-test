56c32cb5b63da34cf7551e39cceaa856923a7a6b
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-09-09 21:51:02 +0530

    [FEAT]: Header actions

--- src/components/Header/HeaderActions.js
+++ src/components/Header/HeaderActions.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const HeaderActions = () => {
-  return <div>HeaderActions</div>;
+  return (
+    <div className="flex items-center gap-6">
+      <button className="header text-base font-semibold py-3 text-[#1A1A1A] leading-5 px-4 flex justify-center items-center rounded-2xl bg-[#F5DF4D] ">
+        {" "}
+        Sign Up Free{" "}
+      </button>
+      <button className="header text-base font-semibold py-3 text-[#1A1A1A] leading-5 px-4 flex justify-center items-center rounded-2xl bg-[#F5DF4D] ">
+        Login <i class="fa-solid ml-3 fa-arrow-right"></i>
+      </button>
+    </div>
+  );
 };
 
 export default HeaderActions;
