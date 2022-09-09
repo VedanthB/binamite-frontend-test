6955ef89116259ecdefc288c5057b797af77fde1
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-09-09 21:41:25 +0530

    [FEAT]: Header Nav

--- src/components/Header/HeaderNav.js
+++ src/components/Header/HeaderNav.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HeaderNav = () => {
-  return <div>HeaderNav</div>;
+  return (
+    <ul className="flex gap-6 text-[#F5DF4D] text-base">
+      <Link to="/">Pricing</Link>
+      <Link to="/">Who's it for?</Link>
+      <Link to="/">Features</Link>
+      <Link to="/">Blog</Link>
+      <Link to="/">
+        <i class="fa-brands fa-instagram"></i>
+      </Link>
+      <Link to="/">
+        <i class="fa-brands fa-facebook"></i>
+      </Link>
+    </ul>
+  );
 };
 
 export default HeaderNav;
